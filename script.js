bf28a3b2ef2ffff03c88b3282d8659205ed29579
Neil Raghav <neil@localhost.localdomain>
2020-05-02 02:34:17 +0530

    project setup

---
+++ script.js
@@ -0,0 +1,69 @@
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function getDeck()
+{
+	var deck = new Array();
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < values.length; x++)
+		{
+			var card = {Value: values[x], Suit: suits[i]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+}
+
+function shuffle(deck)
+{
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+}
+
+function renderDeck(deck)
+{
+      document.getElementById("deck").innerHTML = "";
+
+	for(let i = 0; i < deck.length; i++)
+	{
+		let card = document.createElement("div");
+		let value = document.createElement("div");
+		let suit = document.createElement("div");
+		card.className = "card";
+		value.className = "value";
+		suit.className = "suit " + deck[i].Suit;
+
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		document.getElementById("deck").appendChild(card);
+	}
+}
+
+var initialDeck = getDeck();
+renderDeck(initialDeck);
+
+document.getElementById("ShuffleBtn").addEventListener("click", function(){
+	var shuffledDeck = getDeck();
+	shuffle(shuffledDeck);
+	renderDeck(shuffledDeck);
+});
+
+document.getElementById("ResetBtn").addEventListener("click", function(){
+	renderDeck(initialDeck);
+});
+
+
